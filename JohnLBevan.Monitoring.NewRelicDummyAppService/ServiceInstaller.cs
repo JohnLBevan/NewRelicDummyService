0c1a41cc98138aa52bdf0052771002910158258b
JohnLBevan <JohnLBevan@gmail.com>
2014-11-12 14:40:41 +0000

    Windows Service Skeleton (based on code from http://www.codeproject.com/Articles/106742/Creating-a-simple-Windows-Service)

---
+++ JohnLBevan.Monitoring.NewRelicDummyAppService/ServiceInstaller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ServiceProcess;
+namespace JohnLBevan.Monitoring.NewRelicDummyAppService
+{
+    // <summary>
+    ///     Summary description for ProjectInstaller.
+    /// </summary>
+    [System.ComponentModel.RunInstaller(true)]
+    public class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        /// <summary>
+        ///    Required designer variable.
+        /// </summary>
+        //private System.ComponentModel.Container components;
+        private ServiceInstaller serviceInstaller;
+        private ServiceProcessInstaller serviceProcessInstaller;
+
+        public ProjectInstaller()
+        {
+            // This call is required by the Designer.
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///    Required method for Designer support - do not modify
+        ///    the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceInstaller = new ServiceInstaller();
+            this.serviceProcessInstaller = new ServiceProcessInstaller();
+            // 
+            // serviceInstaller
+            // 
+            this.serviceInstaller.Description = "My Windows Service description";
+            this.serviceInstaller.DisplayName = "My WinService";
+            this.serviceInstaller.ServiceName = "WinService";
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Account = ServiceAccount.LocalService;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            // 
+            // ServiceInstaller
+            // 
+            this.Installers.AddRange(
+                new System.Configuration.Install.Installer[] 
+                {
+                    this.serviceProcessInstaller,
+                    this.serviceInstaller
+                }
+            );
+
+        }
+    }
+}
